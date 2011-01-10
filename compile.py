0abc4aae011bcbe25cbb664b25a87ba63132b4b5
Chris <chris.lewis2@gmail.com>
2011-01-10 19:27:31 +1100

    added licence notices

--- compile.py
+++ compile.py
@@ -1,8 +1,13 @@
+#
+# Program to compress HTML files for niceness.
+# Copyright 2010 Christopher Lewis. All rights reserved.
+#
+
 import re
 import subprocess
 
 # Get the contents of the javascript and html files
-html = open( "index.html", "r" ).read()
+html = open( "tron.html", "r" ).read()
 
 # Remove tabs and newlines from the html file
 html = re.sub( '(\t|\n|\r)', '', html )
@@ -18,8 +23,11 @@ js = "<script>" + subprocess.check_output( closure, shell=True ) + "</script>"
 
 html = html.replace( "</body>", "</body>" + js ) # Insert js before head
 
+# Remove any comments
+html = re.sub( "<!--.*?-->", "", html )
+
 # Also remove </body> and </html> tags (see google.com)
-new = re.sub( r"(</html>|</body>)", "", html )
+html = re.sub( r"(</html>|</body>)", "", html )
 
 html = html.replace( ' type="text/css"', '' ) # Makes no difference to browser
 
--- matrix.js
+++ matrix.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of jsTron.
+ *
+ * jsTron is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jsTron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jsTron.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
 	Create an X by Y matrix and return it
 	@param {number} width
